f7c6c1b2d9f88bd49b5679233a15c211dba168d0
DeathDestructor <ashishpatel9119@hotmail.com>
2021-08-17 18:08:16 +0530

    updated connScan

--- scanning/advportscan.py
+++ scanning/advportscan.py
@@ -1,4 +1,4 @@
-#! usr/bin/python
+#! /bin/python3
 
 from socket import *
 import optparse
@@ -14,15 +14,28 @@ def portScan(tgtHost, tgtPorts):
         
     try:
         tgtName = gethostnamebyaddr(tgtIP)
-        print('[+] Scan Results for:  ' + tgtName[0])
+        print('[*] Scan Results for:  ' + tgtName[0])
     except:
-        print('[+] Scan Results for: ' + tgtIP)
+        print('[*] Scan Results for: ' + tgtIP)
         
     setdefaulttimeout(1)
     
     for tgtPort in tgtPorts:
         t = Thread(target = connScan, args = (tgtHost, int(tgtPort)))
         t.start()
+    
+    
+    
+def connScan(tgtHost, tgtPort):
+    try:
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.connect((tgtHost, tgtPort))
+        print('[+] %d/tcp Open' % tgtPort)
+    except:
+        print('[-] %d/tcp Closed' % tgtPort)
+    finally:
+        sock.close()
+    
     
 
 
@@ -34,17 +47,17 @@ def main():
     parser.add_option('-p', dest = 'tgtPort', type = 'string', 
                         help = 'specify target ports seperated by comma')
 
-    (options, args) = parser.parse.args()
+    (options, args) = parser.parse_args()
     
     tgtHost = options.tgtHost
     tgtPorts = str(options.tgtPort).split(',')
     
-    if (tgtHost == None) | (tgtPorts[0]):
+    if (tgtHost == None) | (tgtPorts[0] == None):
         print(parser.usage)
         exit(0)
     
 
-    #portScan(tgtHost, tgtPorts)
+    portScan(tgtHost, tgtPorts)
 
 if __name__ == '__main__':
     main()
